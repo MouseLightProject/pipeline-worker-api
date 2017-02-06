598a2554c534cdb6c8d7333b8336bee39cf1b867
Patrick Edson <pedson@leapscientific.com>
2017-02-05 21:27:17 -0500

    Reorder

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+LAST_NODE_ENV=${NODE_ENV}
+
+if [ -z "$1" ]; then
+    export NODE_ENV=production
+else
+    export NODE_ENV=${1}
+fi
+
 if [ -z "$2" ]; then
     echo
 else
@@ -8,25 +16,20 @@ fi
 
 if [ -z "$MACHINE_ID" ]; then
     echo "MACHINE_ID must be set or a script provided that sets it as the second argument."
+    NODE_ENV=${LAST_NODE_ENV}
     exit 1
 fi
 
 if [ -z "$WORK_UNIT_CAPACITY" ]; then
     echo "WORK_UNIT_CAPACITY must be set or a script provided that sets it as the second argument."
+    NODE_ENV=${LAST_NODE_ENV}
     exit 2
 fi
 
 if [ -z "$SERVER_HOST" ]; then
     echo "SERVER_HOST must be set or a script provided that sets it as the second argument."
+    NODE_ENV=${LAST_NODE_ENV}
     exit 3
-fi
-
-LAST_NODE_ENV=${NODE_ENV}
-
-if [ -z "$1" ]; then
-    export NODE_ENV=production
-else
-    export NODE_ENV=${1}
 fi
 
 nohup npm run dev &
