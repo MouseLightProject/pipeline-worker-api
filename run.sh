abdabaa973623f7d8be0663b80bec406333eadef
Patrick Edson <pedson@leapscientific.com>
2017-03-05 10:16:50 -0500

    Remove optional env

--- run.sh
+++ run.sh
@@ -16,22 +16,10 @@ if [ -z "$NODE_ENV" ]; then
     NODE_ENV=production
 fi
 
-if [ -z "$MACHINE_ID" ]; then
-    echo "MACHINE_ID must be set or a script provided that sets it as the second argument."
-    NODE_ENV=${LAST_NODE_ENV}
-    exit 1
-fi
-
-if [ -z "$WORK_UNIT_CAPACITY" ]; then
-    echo "WORK_UNIT_CAPACITY must be set or a script provided that sets it as the second argument."
-    NODE_ENV=${LAST_NODE_ENV}
-    exit 2
-fi
-
 if [ -z "$SERVER_HOST" ]; then
     echo "SERVER_HOST must be set or a script provided that sets it as the second argument."
     NODE_ENV=${LAST_NODE_ENV}
-    exit 3
+    exit 1
 fi
 
 nohup npm run dev &
