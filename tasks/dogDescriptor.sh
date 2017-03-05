4123b51ad017a322576ddba4d8130f873a4ab3e8
Patrick Edson <pedson@leapscientific.com>
2017-03-05 14:16:39 -0500

    Incorrect label

--- tasks/dogDescriptor.sh
+++ tasks/dogDescriptor.sh
@@ -21,7 +21,7 @@ input_file1="$pipeline_input_root/$tile_relative_path/$tile_name-prob.0.h5"
 input_file2="$pipeline_input_root/$tile_relative_path/$tile_name-prob.1.h5"
 
 output_file="$pipeline_output_root/$tile_relative_path/$tile_name"
-output_file+="-prob"
+output_file+="-desc"
 output_file1="$output_file.0.txt"
 output_file2="$output_file.1.txt"
 
