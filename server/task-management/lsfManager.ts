18a4871768c214055c0d98597a60da0cfb20a1e1
Patrick Edson <pedson@users.noreply.github.com>
2017-12-18 15:45:51 -0500

    Change to global replace.

--- server/task-management/lsfManager.ts
+++ server/task-management/lsfManager.ts
@@ -102,7 +102,7 @@ export class LSFTaskManager implements ITaskUpdateSource, ITaskManager {
 
         const requiredBsubArgs = ["-J", `ml-dg-${taskExecution.tile_id}`, "-cwd", "-g", `/mouselight/pipeline/${taskExecution.worker_id}`, "-oo", `${taskExecution.resolved_log_path + ".cluster.out.log"}`, "-eo", `${taskExecution.resolved_log_path + ".cluster.err.log"}`, `'${programArgs}'`];
 
-        const clusterArgs = taskExecution.resolved_cluster_arg_array.join(" ").replace(`"`, `\\"`);
+        const clusterArgs = taskExecution.resolved_cluster_arg_array.join(" ").replace(/"/g, `\\"`);
 
         console.log(clusterArgs);
 
